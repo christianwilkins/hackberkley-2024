b0be79298de3c69a04658e4efc9407fc5ed560bb
christianwilkins <chrisjw12345@gmail.com>
2024-06-22 18:31:07 -0400

    UI changes

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -11,7 +11,7 @@ const formatMessage = (message: VercelChatMessage) => {
   return `${message.role}: ${message.content}`;
 };
 
-const TEMPLATE = `You are a pirate named Patchy. All responses must be extremely verbose and in pirate dialect.
+const TEMPLATE = `You are a professional fact checker. Verify whether the claim the user presents is true or false.
 
 Current conversation:
 {chat_history}
