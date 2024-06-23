9fafbebd86ab890eaff8e344c6c857322c729a72
christianwilkins <chrisjw12345@gmail.com>
2024-06-22 20:22:33 -0400

    removed unused files

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,9 +8,8 @@ export function Navbar() {
     <nav className="mb-4">
       <a className={`mr-4 ${pathname === "/landing" ? "text-white border-b" : ""}`} href="/landing">TruthToken</a>
       <a className={`mr-4 ${pathname === "/" ? "text-white border-b" : ""}`} href="/">Verify Claim</a>
-      <a className={`mr-4 ${pathname === "/about" ? "text-white border-b" : ""}`} href="/about">About</a>
       <a className={`mr-4 ${pathname === "/graph" ? "text-white border-b" : ""}`} href="/graph">Graph</a>
-      {/* <a className={`mr-4 ${pathname === "/generative_ui" ? "text-white border-b" : ""}`} href="/generative_ui">✨ Generative UI</a> */}
+      <a className={`mr-4 ${pathname === "/about" ? "text-white border-b" : ""}`} href="/about">About</a>
     </nav>
   );
 }
