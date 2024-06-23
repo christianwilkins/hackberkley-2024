6982f4801aa46bfc3e3d4fb05bd7436a3d620255
christianwilkins <chrisjw12345@gmail.com>
2024-06-23 15:15:21 -0400

    removed unused...

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,8 +8,6 @@ export function Navbar() {
     <nav className="mb-4">
       <a className={`mr-4 ${pathname === "/landing" ? "text-white border-b" : ""}`} href="/landing">true.</a>
       <a className={`mr-4 ${pathname === "/" ? "text-white border-b" : ""}`} href="/">verify claim</a>
-      <a className={`mr-4 ${pathname === "/graph" ? "text-white border-b" : ""}`} href="/graph">graph</a>
-      <a className={`mr-4 ${pathname === "/about" ? "text-white border-b" : ""}`} href="/about">about</a>
     </nav>
   );
 }
